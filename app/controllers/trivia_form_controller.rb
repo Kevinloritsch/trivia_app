48b730c2b6dd3ec5aed8f140402a7024a704d60e
PrematureDeer8 <yankeegabi.torres@gmail.com>
2021-10-19 10:05:51 -0700

    made some random methods

--- app/controllers/trivia_form_controller.rb
+++ app/controllers/trivia_form_controller.rb
@@ -2,11 +2,39 @@ class TriviaFormController < ApplicationController
   def trivia_form
   end
   def submit
-    @question = params
-    # @answer_a = params[:a]
-    # @answer_b = params[:b]
-    # @answer_c = params[:c]
-    # @answer_d = params[:d]
-    puts(params[:question])
+    def get_length_mutiple_choice(input)
+      counter = 0;
+      while(true)
+        if(counter == 0)
+          question_key = "question"
+        else
+          question_key = "question"+counter.to_s
+        end
+        if(input[question_key] == nil)
+          return counter;
+        end
+        counter = counter +1
+      end
+    end
+    def get_length_frq(input)
+      count = 0;
+      while(true)
+        question_key = "frqquestion"+count.to_s
+        if(input[question_key] == nil)
+          return count;
+        end
+        count = count +1
+      end
+    end
+    @inputs = params
+    @mutiple_choice_length = get_length_mutiple_choice(@inputs)
+    @frq_length = get_length_frq(@inputs)
+    question = ""
+    for input in inputs
+      if(input.includes?("question"))
+        question = question + input + "/"+ inputs[input]+ "/";
+      end
+    end
+    
   end
 end
