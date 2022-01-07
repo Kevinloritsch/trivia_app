fd4877fbacbe193797e0c99d8201ce0f1101c6bd
Kevinloritsch <kloritsch58@gmail.com>
2022-01-07 12:16:28 -0800

    Remove dumb things

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,7 +15,6 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import * as THREE from 'three'
 
 
 require("jquery")
