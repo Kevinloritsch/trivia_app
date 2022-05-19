2891a178f370857829bbc8236e020331054da06e
Kevinloritsch <kloritsch58@gmail.com>
2022-05-18 20:10:26 -0700

    New Email API

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'ke-twitter.herokuapp.com'
+  host = 'superap-neatotrivia.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
   :user_name => 'apikey',
