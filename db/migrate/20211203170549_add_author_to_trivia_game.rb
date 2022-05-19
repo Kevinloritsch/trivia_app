b7df794f6fa00a0bd0d10b74960017067bb57412
Kevinloritsch <kloritsch58@gmail.com>
2022-05-19 08:57:00 -0700

    Migration Error

--- db/migrate/20211203170549_add_author_to_trivia_game.rb
+++ db/migrate/20211203170549_add_author_to_trivia_game.rb
@@ -1,9 +1,5 @@
 class AddAuthorToTriviaGame < ActiveRecord::Migration[6.1]
   def change
-<<<<<<< HEAD
-    add_column(:trivia_games, :author, :string)
-=======
     add_column :trivia_games, :author, :string, null: true
->>>>>>> 25f234b04e74994a449194c9d057cbc931fde580
   end
 end
